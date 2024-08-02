9552e9a5b2774518d3ba37efc94a4d39aec64bb6
Keith Kester <keith.a.kester.ctr@army.mil>
2024-08-02 15:31:05 -0500

    removed async

--- src/main/java/com/example/asyncdemo/AsyncDemoApplication.java
+++ src/main/java/com/example/asyncdemo/AsyncDemoApplication.java
@@ -2,11 +2,9 @@ package com.example.asyncdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableAsync
 @EnableScheduling
 public class AsyncDemoApplication {
 
